bae7a487845c52180304ea9f8bf6cc8c7a5d5c69
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-12-26 17:03:11 -0600

    Adds most of the header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,33 +1,68 @@
+'use client';
+
 import { Button } from '@/app/components/ui/button';
 import Link from 'next/link';
 import React from 'react';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
-    <header className="w-full flex flex-row justify-between">
+    <header className="items-center w-full flex flex-row justify-between px-32 h-28">
       <div>Logo Goes Here</div>
 
       <nav>
         <ul className="flex flex-row gap-16">
-          <li>
+          <li
+            className={`${
+              pathname === '/'
+                ? 'text-green-500 font-bold'
+                : 'text-black font-medium'
+            }`}
+          >
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li
+            className={`${
+              pathname === '/explore'
+                ? 'text-green-500 font-bold'
+                : 'text-black font-medium'
+            }`}
+          >
             <Link href="/explore">Explore</Link>
           </li>
-          <li>
+          <li
+            className={`${
+              pathname === '/rooms'
+                ? 'text-green-500 font-bold'
+                : 'text-black font-medium'
+            }`}
+          >
             <Link href="/rooms">Rooms</Link>
           </li>
-          <li>
+          <li
+            className={`${
+              pathname === '/about'
+                ? 'text-green-500 font-bold'
+                : 'text-black font-medium'
+            }`}
+          >
             <Link href="/about">About</Link>
           </li>
-          <li>
+          <li
+            className={`${
+              pathname === '/contact'
+                ? 'text-green-500 font-medium'
+                : 'text-black font-medium'
+            }`}
+          >
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
 
-      <Button>Book now</Button>
+      <Button className="h-14 rounded bg-green-500 px-11">Book now</Button>
     </header>
   );
 };
